410c16a119cf878deca72a76e0177f28963a16ce
Dipto-at-sap <dipto.das@sap.com>
2025-12-23 21:47:40 +0530

    feat: add CI pipeline

---
+++ test.js
@@ -0,0 +1,7 @@
+console.log("Starting DevOps Pipeline test...");
+
+setTimeout(() => {
+    console.log("Waiting 3 seconds to simulate pipeline delay...");
+}, 3000);
+
+console.log("DevOps Pipeline test completed.");
